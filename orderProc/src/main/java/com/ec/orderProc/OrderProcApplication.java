1684202713dbdce2d5e463ff3649d567aaf3b8ea
meetrami <meetrami905@gmail.com>
2026-08-01 17:34:45 +0530

    initial commit

---
+++ orderProc/src/main/java/com/ec/orderProc/OrderProcApplication.java
@@ -0,0 +1,13 @@
+package com.ec.orderProc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcApplication.class, args);
+	}
+
+}
---
+++ orderProc/src/test/java/com/ec/orderProc/OrderProcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.orderProc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
